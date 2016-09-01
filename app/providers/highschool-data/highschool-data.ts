12f1b8766e122dd45f48665a867424dde21fb34c
Jorge Vergara <javebratt@gmail.com>
2016-09-01 07:22:23 -0500

    Fixes security rules and adds multi node update for selecting high school

--- app/providers/highschool-data/highschool-data.ts
+++ app/providers/highschool-data/highschool-data.ts
@@ -4,12 +4,12 @@ import * as firebase from 'firebase';
 @Injectable()
 export class HighschoolData {
   public fireSchool: any;
-  public userProfileRef: any;
+  public rootRef: any;
   public currentUser: any;
 
   constructor() {
     this.fireSchool = firebase.database().ref('/highSchools');
-    this.userProfileRef = firebase.database().ref('/userProfile/');
+    this.rootRef = firebase.database().ref('/');
     this.currentUser = firebase.auth().currentUser;
   }
 
@@ -28,15 +28,17 @@ export class HighschoolData {
   addSchool(state, city, schoolName, schoolId): any {
     let updates = {};
 
-    this.fireSchool.child(state).child(city).child('schoolList').child(schoolId)
-      .child('students').child(this.currentUser.uid).set(true);
+    // this.fireSchool.child(state).child(city).child('schoolList').child(schoolId)
+    //   .child('students').child(this.currentUser.uid).set(true);
 
-    updates[this.currentUser.uid + '/highSchool/city'] = city;
-    updates[this.currentUser.uid + '/highSchool/name'] = schoolName;
-    updates[this.currentUser.uid + '/highSchool/state'] = state;
-    updates[this.currentUser.uid + '/highSchool/id'] = schoolId;
+    updates['highSchools/' + state + '/' + city + '/schoolList/' + schoolId
+    + '/students/' + this.currentUser.uid] = true;
+    updates['userProfile/' + this.currentUser.uid + '/highSchool/city'] = city;
+    updates['userProfile/' + this.currentUser.uid + '/highSchool/name'] = schoolName;
+    updates['userProfile/' + this.currentUser.uid + '/highSchool/state'] = state;
+    updates['userProfile/' + this.currentUser.uid + '/highSchool/id'] = schoolId;
 
-    return this.userProfileRef.update(updates);
+    return this.rootRef.update(updates);
 
   }
 
